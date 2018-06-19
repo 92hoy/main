70fe4455d4abeaca8611e5ec22d6d77205a06112
ahn <93immm@naver.com>
2018-06-19 16:44:14 +0900

    sql -> model

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# from backend.models import CmsManager
 class CmsManager(models.Model):
     user_id = models.TextField(primary_key=True)
     user_pwd = models.TextField()
@@ -14,7 +15,373 @@ class CmsManager(models.Model):
     regist_id = models.TextField()
     modify_date = models.DateTimeField()
     modify_id = models.TextField()
-
     class Meta:
         managed = False
         db_table = 'cms_manager'
+
+# from backend.models import ApiCdrCall
+class ApiCdrCall(models.Model):
+    session = models.TextField(primary_key=True)
+    correlatorindex = models.IntegerField(primary_key=True)
+    record_type = models.TextField()
+    call_id = models.TextField()
+    name = models.TextField()
+    calltype = models.TextField()
+    cospace = models.TextField()
+    callcorrelator = models.TextField()
+    calllegscompleted = models.IntegerField()
+    calllegsmaxactive = models.IntegerField()
+    durationseconds = models.IntegerField()
+    add_date = models.DateTimeField()
+    cdrtag = models.TextField()
+    tenant = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'api_cdr_call'
+
+# from backend.models import ApiCdrCallleg
+class ApiCdrCallleg(models.Model):
+    session = models.TextField(primary_key=True)
+    correlatorindex = models.IntegerField(primary_key=True)
+    record_type = models.TextField()
+    callleg_id = models.TextField()
+    cdrtag = models.TextField()
+    displayname = models.TextField()
+    guestconnection = models.TextField()
+    localaddress = models.TextField()
+    remoteaddress = models.TextField()
+    remoteparty = models.TextField()
+    recording = models.TextField()
+    type = models.TextField()
+    subtype = models.TextField()
+    lyncsubtype = models.TextField()
+    direction = models.TextField()
+    call = models.TextField()
+    ivr = models.TextField()
+    ownerid = models.TextField()
+    sipcallid = models.TextField()
+    groupid = models.TextField()
+    reason = models.TextField()
+    remoteteardown = models.TextField()
+    encryptedmedia = models.TextField()
+    unencryptedmedia = models.TextField()
+    durationseconds = models.IntegerField()
+    mediausagepercentages_mainvideoviewer = models.FloatField()
+    mediausagepercentages_mainvideocontributor = models.FloatField()
+    mediausagepercentages_presentationviewer = models.FloatField()
+    mediausagepercentages_presentationcontributor = models.FloatField()
+    alarm_type = models.TextField()
+    alarm_durationpercentage = models.FloatField()
+    rxvideo_codec = models.TextField()
+    rxvideo_maxsizewidth = models.IntegerField()
+    rxvideo_maxsizeheight = models.IntegerField()
+    rxvideo_packetlossbursts_duration = models.IntegerField()
+    rxvideo_packetlossbursts_density = models.IntegerField()
+    rxvideo_packetgap_duration = models.IntegerField()
+    rxvideo_packetgap_density = models.IntegerField()
+    txvideo_codec = models.TextField()
+    txvideo_maxsizewidth = models.IntegerField()
+    txvideo_maxsizeheight = models.IntegerField()
+    txvideo_packetlossbursts_duration = models.FloatField()
+    txvideo_packetlossbursts_density = models.FloatField()
+    txvideo_packetgap_duration = models.FloatField()
+    txvideo_packetgap_density = models.FloatField()
+    rxaudio_codec = models.TextField()
+    rxaudio_packetlossbursts_duration = models.FloatField()
+    rxaudio_packetlossbursts_density = models.FloatField()
+    rxaudio_packetgap_duration = models.FloatField()
+    rxaudio_packetgap_density = models.FloatField()
+    txaudio_codec = models.TextField()
+    txaudio_packetlossbursts_duration = models.FloatField()
+    txaudio_packetlossbursts_density = models.FloatField()
+    txaudio_packetgap_duration = models.FloatField()
+    txaudio_packetgap_density = models.FloatField()
+    state = models.TextField()
+    deactivated = models.TextField()
+    add_date = models.DateTimeField()
+    ep_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'api_cdr_callleg'
+
+# from backend.models import ApiCdrRecord
+class ApiCdrRecord(models.Model):
+    session = models.TextField(primary_key=True)
+    correlatorindex = models.IntegerField(primary_key=True)
+    type = models.TextField()
+    time = models.TextField()
+    recordindex = models.IntegerField()
+    add_date = models.DateTimeField()
+    utc_time = models.DateTimeField()
+    local_time = models.DateTimeField()
+    class Meta:
+        managed = False
+        db_table = 'api_cdr_record'
+
+# from backend.models import ApiCdrRecording
+class ApiCdrRecording(models.Model):
+    session = models.TextField(primary_key=True)
+    correlatorindex = models.IntegerField(primary_key=True)
+    record_type = models.TextField()
+    recording_id = models.TextField()
+    path = models.TextField()
+    recorderurl = models.TextField()
+    call = models.TextField()
+    callleg = models.TextField()
+    add_date = models.DateTimeField()
+    class Meta:
+        managed = False
+        db_table = 'api_cdr_recording'
+
+# from backend.models import ApiCdrRecords
+class ApiCdrRecords(models.Model):
+    session = models.TextField(primary_key=True)
+    callbridge = models.TextField()
+    add_date = models.DateTimeField()
+    class Meta:
+        managed = False
+        db_table = 'api_cdr_records'
+
+# from backend.models import CmsCospace
+class CmsCospace(models.Model):
+    cospace_seq = models.TextField(primary_key=True)
+    cospace_id = models.TextField()
+    start_dt = models.DateTimeField()
+    passcode = models.TextField()
+    call_id = models.TextField()
+    bandwidth = models.TextField()
+    delete_yn = models.TextField()
+    use_yn = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_cospace'
+
+# from backend.models import CmsCospaceControl
+class CmsCospaceControl(models.Model):
+    cospace_seq = models.IntegerField(primary_key=True)
+    cospace_id = models.TextField(primary_key=True)
+    id = models.TextField()
+    delete_yn = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_cospace_control'
+
+# from backend.models import CmsCospaceDetail
+class CmsCospaceDetail(models.Model):
+    cospace_id = models.TextField(primary_key=True)
+    user_id = models.TextField(primary_key=True)
+    class Meta:
+        managed = False
+        db_table = 'cms_cospace_detail'
+
+# from backend.models import CmsCospaceEndpoint
+class CmsCospaceEndpoint(models.Model):
+    seq = models.IntegerField(primary_key=True)
+    callnumber = models.TextField()
+    id = models.TextField()
+    type = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_cospace_endpoint'
+
+# from backend.models import CmsCospaceUser
+class CmsCospaceUser(models.Model):
+    seq = models.IntegerField(primary_key=True)
+    userjid = models.TextField(primary_key=True)
+    name = models.TextField()
+    email = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_cospace_user'
+
+# from backend.models import CmsEndpoint
+class CmsEndpoint(models.Model):
+    ep_id = models.TextField(primary_key=True)
+    ep_group_seq = models.TextField()
+    ep_name = models.TextField()
+    ep_type = models.TextField()
+    ip = models.TextField()
+    sip = models.TextField()
+    username = models.TextField()
+    recodingdevice = models.TextField()
+    audioonly = models.TextField()
+    hdevice = models.TextField()
+    mslync = models.TextField()
+    gmt_time = models.TextField()
+    ep_group_sub_seq = models.TextField()
+    order_no = models.IntegerField()
+    delete_yn = models.TextField()
+    use_yn = models.TextField()
+    open_yn = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_endpoint'
+
+# from backend.models import CmsEndpointGroup
+class CmsEndpointGroup(models.Model):
+    ep_group_seq = models.TextField(primary_key=True)
+    ep_grroup_name = models.TextField()
+    ep_group_color = models.TextField()
+    ep_order = models.IntegerField()
+    delete_yn = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_endpoint_group'
+
+# from backend.models import CmsLdapserver
+class CmsLdapserver(models.Model):
+    id = models.TextField(primary_key=True)
+    title = models.TextField()
+    password = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_ldapserver'
+
+# from backend.models import CmsResvCospace
+class CmsResvCospace(models.Model):
+    resv_seq = models.IntegerField(primary_key=True)
+    start_date = models.DateTimeField()
+    resv_name = models.TextField()
+    end_date = models.DateTimeField()
+    passcode = models.TextField()
+    call_id = models.TextField()
+    bandwidth = models.TextField()
+    delete_yn = models.TextField()
+    use_yn = models.TextField()
+    call_yn = models.TextField()
+    discon_yn = models.TextField()
+    call_uuid = models.TextField()
+    cospace_uuid = models.TextField()
+    schedule_call_type = models.TextField()
+    host_name = models.TextField()
+    host_email = models.TextField()
+    cospace_type = models.TextField()
+    cospace_url = models.TextField()
+    duration = models.IntegerField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_resv_cospace'
+
+# from backend.models import CmsResvCospaceEndpoint
+class CmsResvCospaceEndpoint(models.Model):
+    resv_seq = models.IntegerField(primary_key=True)
+    ep_id = models.IntegerField(primary_key=True)
+    call_yn = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_resv_cospace_endpoint'
+
+# from backend.models import CmsResvCospaceUser
+class CmsResvCospaceUser(models.Model):
+    resv_seq = models.IntegerField(primary_key=True)
+    userjid = models.TextField(primary_key=True)
+    name = models.TextField()
+    email = models.TextField()
+    call_yn = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_resv_cospace_user'
+
+# from backend.models import CmsTemplate
+class CmsTemplate(models.Model):
+    seq = models.IntegerField(primary_key=True)
+    title = models.TextField()
+    delete_yn = models.TextField()
+    callbrandingprofile = models.TextField()
+    calllegprofile = models.TextField()
+    callprofile = models.TextField()
+    dtmfprofile = models.TextField()
+    ivrbrandingprofile = models.TextField()
+    userprofile = models.TextField()
+    note = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_template'
+
+# from backend.models import CmsCodeGroup
+class CmsCodeGroup(models.Model):
+    group_code = models.TextField(primary_key=True)
+    group_name = models.TextField()
+    group_ename = models.TextField()
+    group_note = models.TextField()
+    delete_yn = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_code_group'
+
+# from backend.models import CmsCodeDetail
+class CmsCodeDetail(models.Model):
+    group_code = models.TextField(primary_key=True)
+    detail_code = models.TextField(primary_key=True)
+    code_name = models.TextField()
+    code_ename = models.TextField()
+    note = models.TextField()
+    ref_code = models.TextField()
+    order_no = models.IntegerField()
+    delete_yn = models.TextField()
+    regist_date = models.DateTimeField()
+    regist_id = models.TextField()
+    modify_date = models.DateTimeField()
+    modify_id = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'cms_code_detail'
+
+# from backend.models import CmsLoginLog
+class CmsLoginLog(models.Model):
+    seq = models.IntegerField(primary_key=True)
+    user_id = models.TextField()
+    login_ip = models.TextField()
+    user_agent = models.TextField()
+    regist_date = models.DateTimeField()
+    class Meta:
+        managed = False
+        db_table = 'cms_login_log'
--- make.sql
+++ make.sql
@@ -1,27 +1,345 @@
 create table cms_manager(
-	user_id varchar(50) primary key not null,
-    user_name varchar(50)  not null,
-    user_pwd varchar(100)  not null,
-    user_role varchar(100)  not null,
-    pw_change_date datetime default null,
-    login_fail_cnt int default 0,
-    last_login datetime default null,
-    language varchar(20) default 'en',
-    delete_yn char(1) default 'N',
-    regist_date datetime default now(),
-    regist_id varchar(50) default null,
-    modify_date datetime default null,
-    modify_id varchar(50) default null
+	`user_id` varchar(50)  not null,
+	`user_name` varchar(50)  not null,
+	`user_pwd` varchar(100)  not null,
+	`user_role` varchar(100)  not null,
+	`pw_change_date` datetime default null,
+	`login_fail_cnt` int default 0,
+	`last_login` datetime default null,
+	`language` varchar(20) default 'en',
+	`delete_yn` char(1) default 'N',
+	`regist_date` datetime default now(),
+	`regist_id` varchar(50) default null,
+	`modify_date` datetime default null,
+	`modify_id` varchar(50) default null,
+	primary key(user_id)
 );
 
-insert into cms_manager(user_id, user_name, user_pwd, user_role)
-values('system', '안진용', 'edx', 'S');
+create table api_cdr_call(
+	`session`  varchar(50)   ,
+	`correlatorindex`  int   ,
+	`record_type`  varchar(100)   ,
+	`call_id`  varchar(100)   ,
+	`name`  varchar(100)   ,
+	`calltype`  varchar(100)   ,
+	`cospace`  varchar(100)   ,
+	`callcorrelator`  varchar(100)   ,
+	`calllegscompleted`  int   ,
+	`calllegsmaxactive`  int   ,
+	`durationseconds`  int   ,
+	`add_date`  datetime   ,
+	`cdrtag`  varchar(100)   ,
+	`tenant`  varchar(100)   ,
+	primary key ( session, correlatorindex )
+);
 
-insert into cms_manager(user_id, user_name, user_pwd, user_role)
-values('admin', '조호영', 'edx', 'A');
+create table api_cdr_callleg(
+	`session`  varchar(100)   ,
+	`correlatorindex`  int   ,
+	`record_type`  varchar(100)   ,
+	`callleg_id`  varchar(100)   ,
+	`cdrtag`  varchar(100)   ,
+	`displayname`  varchar(100)   ,
+	`guestconnection`  varchar(100)   ,
+	`localaddress`  varchar(100)   ,
+	`remoteaddress`  varchar(100)   ,
+	`remoteparty`  varchar(100)   ,
+	`recording`  varchar(100)   ,
+	`type`  varchar(100)   ,
+	`subtype`  varchar(100)   ,
+	`lyncsubtype`  varchar(100)   ,
+	`direction`  varchar(100)   ,
+	`call`  varchar(100)   ,
+	`ivr`  varchar(100)   ,
+	`ownerid`  varchar(100)   ,
+	`sipcallid`  varchar(100)   ,
+	`groupid`  varchar(100)   ,
+	`reason`  varchar(100)   ,
+	`remoteteardown`  varchar(100)   ,
+	`encryptedmedia`  varchar(100)   ,
+	`unencryptedmedia`  varchar(100)   ,
+	`durationseconds`  int   ,
+	`mediausagepercentages_mainvideoviewer`  double   ,
+	`mediausagepercentages_mainvideocontributor`  double   ,
+	`mediausagepercentages_presentationviewer`  double   ,
+	`mediausagepercentages_presentationcontributor`  double   ,
+	`alarm_type`  varchar(100)   ,
+	`alarm_durationpercentage`  double   ,
+	`rxvideo_codec`  varchar(100)   ,
+	`rxvideo_maxsizewidth`  int   ,
+	`rxvideo_maxsizeheight`  int   ,
+	`rxvideo_packetlossbursts_duration`  double   ,
+	`rxvideo_packetlossbursts_density`  double   ,
+	`rxvideo_packetgap_duration`  double   ,
+	`rxvideo_packetgap_density`  double   ,
+	`txvideo_codec`  varchar(100)   ,
+	`txvideo_maxsizewidth`  int   ,
+	`txvideo_maxsizeheight`  int   ,
+	`txvideo_packetlossbursts_duration`  double   ,
+	`txvideo_packetlossbursts_density`  double   ,
+	`txvideo_packetgap_duration`  double   ,
+	`txvideo_packetgap_density`  double   ,
+	`rxaudio_codec`  varchar(100)   ,
+	`rxaudio_packetlossbursts_duration`  double   ,
+	`rxaudio_packetlossbursts_density`  double   ,
+	`rxaudio_packetgap_duration`  double   ,
+	`rxaudio_packetgap_density`  double   ,
+	`txaudio_codec`  varchar(100)   ,
+	`txaudio_packetlossbursts_duration`  double   ,
+	`txaudio_packetlossbursts_density`  double   ,
+	`txaudio_packetgap_duration`  double   ,
+	`txaudio_packetgap_density`  double   ,
+	`state`  varchar(100)   ,
+	`deactivated`  varchar(100)   ,
+	`add_date`  datetime   ,
+	`ep_id`  varchar(100)   ,
+	primary key ( session, correlatorindex )
+);
 
-insert into cms_manager(user_id, user_name, user_pwd, user_role)
-values('user', '안이형', 'edx', 'U');
 
-insert into cms_manager(user_id, user_name, user_pwd, user_role)
-values('monitor', '이원도', 'edx', 'M');
+create table api_cdr_record(
+	`session`  varchar(100)   ,
+	`correlatorindex`  int   ,
+	`type`  varchar(100)   ,
+	`time`  varchar(100)   ,
+	`recordindex`  int   ,
+	`add_date`  datetime   ,
+	`utc_time`  datetime   ,
+	`local_time`  datetime   ,
+	primary key ( session, correlatorindex )
+);
+
+create table api_cdr_recording(
+	`session`  varchar(100)   ,
+	`correlatorindex`  int   ,
+	`record_type`  varchar(100)   ,
+	`recording_id`  varchar(100)   ,
+	`path`  varchar(100)   ,
+	`recorderurl`  varchar(100)   ,
+	`call`  varchar(100)   ,
+	`callleg`  varchar(100)   ,
+	`add_date`  datetime   ,
+	primary key ( session, correlatorindex )
+);
+
+create table api_cdr_records(
+	`session`  varchar(100)   ,
+	`callbridge`  varchar(100)   ,
+	`add_date`  datetime   ,
+	primary key ( session )
+);
+
+
+create table cms_cospace(
+	`cospace_seq`  int  auto_increment ,
+	`cospace_id`  varchar(100)   ,
+	`start_dt`  datetime   ,
+	`passcode`  varchar(100)   ,
+	`call_id`  varchar(100)   ,
+	`bandwidth`  varchar(100)   ,
+	`delete_yn`  varchar(100)   ,
+	`use_yn`  varchar(100)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(100)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(100)   ,
+	primary key ( cospace_seq )
+);
+
+create table cms_cospace_control(
+	`cospace_seq`  int   ,
+	`cospace_id`  varchar(100)   ,
+	`id`  varchar(100)   ,
+	`delete_yn`  varchar(100)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(100)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(100)   ,
+	primary key ( cospace_seq, cospace_id )
+);
+
+create table cms_cospace_detail(
+	`cospace_id`  varchar(100)   ,
+	`user_id`  varchar(100)   ,
+	primary key ( cospace_id, user_id )
+);
+
+create table cms_cospace_endpoint(
+	`seq`  int   ,
+	`callnumber`  varchar(100)   ,
+	`id`  varchar(100)   ,
+	`type`  varchar(100)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(100)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(100)   ,
+	primary key ( seq )
+);
+
+create table cms_cospace_user(
+	`seq`  int   ,
+	`userjid`  varchar(100)   ,
+	`name`  varchar(100)   ,
+	`email`  varchar(100)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(100)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(100)   ,
+	primary key ( seq, userjid )
+);
+
+ create table cms_endpoint(
+	`ep_id`  varchar(100)   ,
+	`ep_group_seq`  varchar(100)   ,
+	`ep_name`  varchar(100)   ,
+	`ep_type`  varchar(100)   ,
+	`ip`  varchar(100)   ,
+	`sip`  varchar(100)   ,
+	`username`  varchar(100)   ,
+	`recodingdevice`  varchar(100)   ,
+	`audioonly`  varchar(100)   ,
+	`hdevice`  varchar(100)   ,
+	`mslync`  varchar(100)   ,
+	`gmt_time`  varchar(100)   ,
+	`ep_group_sub_seq`  varchar(100)   ,
+	`order_no`  int   ,
+	`delete_yn`  varchar(100)   ,
+	`use_yn`  char(1)   ,
+	`open_yn`  char(1)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(50)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(50)   ,
+	primary key ( ep_id )
+);
+
+create table cms_endpoint_group(
+	`ep_group_seq`  varchar(100)   ,
+	`ep_grroup_name`  varchar(100)   ,
+	`ep_group_color`  varchar(100)   ,
+	`ep_order`  int   ,
+	`delete_yn`  char(1)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(50)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(50)   ,
+	primary key ( ep_group_seq )
+);
+
+create table cms_ldapserver(
+	`id`  varchar(100)   ,
+	`title`  varchar(100)   ,
+	`password`  varchar(100)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(50)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(50)   ,
+	primary key ( id )
+);
+
+create table cms_resv_cospace(
+	`resv_seq`  int   ,
+	`start_date`  datetime   ,
+	`resv_name`  varchar(100)   ,
+	`end_date`  datetime   ,
+	`passcode`  varchar(100)   ,
+	`call_id`  varchar(100)   ,
+	`bandwidth`  varchar(100)   ,
+	`delete_yn`  char(1)   ,
+	`use_yn`  char(1)   ,
+	`call_yn`  char(1)   ,
+	`discon_yn`  char(1)   ,
+	`call_uuid`  varchar(300)   ,
+	`cospace_uuid`  varchar(300)   ,
+	`schedule_call_type`  varchar(50)   ,
+	`host_name`  varchar(300)   ,
+	`host_email`  varchar(100)   ,
+	`cospace_type`  varchar(100)   ,
+	`cospace_url`  varchar(2000)   ,
+	`duration`  int   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(50)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(50)   ,
+	primary key ( resv_seq )
+);
+
+create table cms_resv_cospace_endpoint(
+	`resv_seq`  int   ,
+	`ep_id`  int   ,
+	`call_yn`  char(1)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(50)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(50)   ,
+	primary key ( resv_seq, ep_id )
+);
+
+create table cms_resv_cospace_user(
+	`resv_seq`  int   ,
+	`userjid`  varchar(200)   ,
+	`name`  varchar(200)   ,
+	`email`  varchar(200)   ,
+	`call_yn`  char(1)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(50)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(50)   ,
+	primary key ( resv_seq, userjid )
+);
+
+create table cms_template(
+	`seq`  int   ,
+	`title`  varchar(100)   ,
+	`delete_yn`  char(1)   ,
+	`callbrandingprofile`  varchar(100)   ,
+	`calllegprofile`  varchar(100)   ,
+	`callprofile`  varchar(100)   ,
+	`dtmfprofile`  varchar(100)   ,
+	`ivrbrandingprofile`  varchar(100)   ,
+	`userprofile`  varchar(100)   ,
+	`note`  varchar(1000)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(50)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(50)   ,
+	primary key ( seq )
+);
+
+create table cms_code_group(
+	`group_code`  varchar(20)   ,
+	`group_name`  varchar(100)   ,
+	`group_ename`  varchar(100)   ,
+	`group_note`  varchar(500)   ,
+	`delete_yn`  char(1)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(50)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(50)   ,
+	primary key ( group_code )
+);
+
+create table cms_code_detail(
+	`group_code`  varchar(20)   ,
+	`detail_code`  varchar(20)   ,
+	`code_name`  varchar(100)   ,
+	`code_ename`  varchar(100)   ,
+	`note`  varchar(500)   ,
+	`ref_code`  varchar(20)   ,
+	`order_no`  int   ,
+	`delete_yn`  char(1)   ,
+	`regist_date`  datetime   ,
+	`regist_id`  varchar(50)   ,
+	`modify_date`  datetime   ,
+	`modify_id`  varchar(50)   ,
+	primary key ( group_code, detail_code )
+);
+
+create table cms_login_log(
+	`seq`  int  auto_increment ,
+	`user_id`  char(8)   ,
+	`login_ip`  varchar(20)   ,
+	`user_agent`  varchar(100)   ,
+	`regist_date`  datetime   ,
+	primary key ( seq )
+);
